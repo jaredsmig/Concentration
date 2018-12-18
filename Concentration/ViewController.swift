b17bb0ce234e911fa0598720ac71c7ea63e3e008
Jared Smigelski <jared.smigelski@gmail.com>
2018-12-18 12:35:46 -0500

    adding a button with an empty flip definition

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        
+    }
 }
 
